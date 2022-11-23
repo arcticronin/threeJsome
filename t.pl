895c84215067ddbc3127609bce1d5e14a151e2ff
arcticronin <ronin.git@gmail.com>
2022-11-23 02:50:30 +0100

    first dommit

---
+++ t.pl
@@ -0,0 +1,26 @@
+readit(Name) :-
+    open(Name, read, Str),
+    read_file(Str, Lines),
+    close(Str),
+    write(Lines), n1.
+
+printit(What):-
+    open('input.txt', read, Str),
+    read(In, Lines),
+    close(In).
+
+read_file(Stream, []) :-
+    at_end_of_stream(Stream).
+
+read_file(Stream, [X | Xs]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream, X),
+    read_file(Stream, Xs).
+
+jsonparse().
+
+picchia("Davi", "saraH").
+piange("sarah").
+
+parla("Sarah"):-
+    pichia("Davi", "Sarah").
