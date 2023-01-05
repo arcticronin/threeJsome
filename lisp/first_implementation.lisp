b327a8f3517af6758273a9190470193e10c160ce
chengjie <chengzhou98@gmail.com>
2023-01-05 17:43:53 +0100

    Begin brackets checker

--- lisp/first_implementation.lisp
+++ lisp/first_implementation.lisp
@@ -1,4 +1,23 @@
+(defparameter curly-brackets '(#\{ #\}))
+(defparameter squared-brackets '(#\[ #\]))
+
+(defparameter test-json-string "{\"nome\" : \"Arthur\",\"cognome\" : \"Dent\"}")
+
 ;;;; Given a string returns an array of characters
-(defun string-to-list (json_string)
-    (coerce json_string 'list))
+(defun string-to-list (json-string)
+    (coerce json-string 'list))
+
+;;;; Given a list of strings returns a list with istances of "strings-to-filter" only
+(defun filter-string-list (string-list strings-to-filter) 
+    (remove-if (lambda (char) 
+                        (not (member char strings-to-filter))) 
+    string-list))
+
+;;;; Input : Json string as list - Checks the number of { == }
+(defun brackets-checker (string-list) 
+    (and 
+    (evenp (list-length (filter-string-list string-list curly-brackets)))
+    (evenp (list-length (filter-string-list string-list squared-brackets)))
+    ))
+
 
