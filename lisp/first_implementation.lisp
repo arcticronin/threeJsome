cb31ab55e8fca3055ef1357592a333792d16ab74
chengjie <chengzhou98@gmail.com>
2023-01-08 12:00:47 +0100

    improved remove white spaces

--- lisp/first_implementation.lisp
+++ lisp/first_implementation.lisp
@@ -1,6 +1,7 @@
 (defparameter curly-brackets '(#\{ #\}))
 (defparameter squared-brackets '(#\[ #\]))
 (defparameter spaces '(#\Space #\Newline #\Tab))
+(defparameter apix '(#\" #\'))
 ;;; NOTE ! #\{carattere_a_piacere} <- è la notazione per i caratteri in lisp
 
 
@@ -10,59 +11,34 @@
      json-string 
      'list))
 
-;;;; Given a list of strings returns a list with istances of "strings-to-filter" only
-(defun filter-string-list (string-list strings-to-filter)
-  (remove-if
-   (lambda (char)
-     (not 
-      (member 
-       char 
-       strings-to-filter))) 
-   string-list))
 
-;;;; Given json string list checks if every bracket matches.
-(defun brackets-checker (string-list)
-  (and
-   (evenp 
-    (list-length 
-     (filter-string-list 
-      string-list
-      curly-brackets)))
-   (evenp 
-    (list-length 
-     (filter-string-list 
-      string-list 
-      squared-brackets))
-)))
-
-;;;; Removing every white spaces
-(defun remove-white-spaces (char-list) 
-  (remove-if
-   (lambda (char)
-     (member char spaces))
-   char-list))
-
-;;;; Given a JSON STRING returns an object
-(defun json-parse (json-string) 
-  (let ((json-string-list 
-         (string-to-list json-string))) 
-    (if (not (brackets-checker 
-              json-string-list))
-        (error "Syntax error! Unbalanced braces")
-      ()
-      )
-    ))
+(defun remove-white-spaces (char-list is-in-string) 
+  (if (null char-list) char-list
+    (let ((list-head (first char-list))
+          (list-body (rest char-list)))
+      (if (null is-in-string)
+          (cond ((eql list-head #\")
+                 (append (list list-head) 
+                         (remove-white-spaces list-body T)))
+                ((member list-head spaces) 
+                 (remove-white-spaces list-body nil))
+                (T (append (list list-head) 
+                           (remove-white-spaces
+                            list-body nil)))
+       )
+       (if (eql list-head #\") 
+           (append (list list-head) 
+                   (remove-white-spaces 
+                    list-body 
+                    nil)) 
+         (append (list list-head) 
+                 (remove-white-spaces 
+                  list-body 
+                  T)))
+       ))))
 
 
-;;;; Given a json-string list determine the initial structure
 
-(defun parse-json-list (json-string-list) 
-  (if (eql 
-       (first json-string-list) 
-       #\{)
-      (list 
-       'jsonobj)
-    (list
-     'jsonarray)))
+
 
 
