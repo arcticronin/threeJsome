be850c2c967ea3ebb0835aac28795ddab34bbd5a
Tmont28 <61681306+Tmont28@users.noreply.github.com>
2023-01-15 22:28:47 +0100

    Update jsonparse.pl

--- Consegna/Prolog/jsonparse.pl
+++ Consegna/Prolog/jsonparse.pl
@@ -1,1 +1,20 @@
+%%%% Membri del Gruppo
+%%%% 856095 Manzi Luca 
+%%%% 851849 Montoli Matteo
+%%%% 859246 Zhou Chengjie
+
+%%%% -*- Mode: Prolog -*-
+
+%%%% begin-of-file json-parsing.pl
+
+
+	
+	 
+
+
+
+
+
+
+%%%% end-of-file json-parsing.pl
 
