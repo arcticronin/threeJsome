1f882368a47429d6dfb8b39d2cdea686502e342e
chengjie <chengzhou98@gmail.com>
2023-01-15 23:22:45 +0100

    Added json access

--- Consegna/Lisp/jsonparse.lisp
+++ Consegna/Lisp/jsonparse.lisp
@@ -477,5 +477,28 @@
                      (stream-to-string inputstream)))))
 ;;; END OF INPUT OUTPUT
 
+;;; BEGIN JSON-ACCESS
+(defun jsonaccess (obj &rest key)
+  (jsonaccess_ obj key)
+  )
+
+(defun jsonaccess_ (obj fields)
+  (if (null fields)
+      obj
+    (let* ((cur-key (first fields))
+           (current 
+            (cond ((and (integerp cur-key) 
+                        (eq (first obj) 'jsonarray))
+                   (nth (+ cur-key 1) obj)) 
+                  ((and (stringp cur-key) 
+                        (eq (first obj) 'jsonobj))
+                   (second (assoc cur-key
+                                  (rest obj) :test #'string=)))
+                  (t (error
+                      "not a JSON object or array or not appropriate key"
+                      obj cur-key)))))
+      (jsonaccess_ current (rest fields))))) 
+;;; END OF JSON-ACCESS
+
 
 ;;; end-of-file jsonparsing.lisp
--- lisp/jsonaccessnew.lisp
+++ lisp/jsonaccessnew.lisp
@@ -1,38 +1,24 @@
 
 
-(defun jsonaccess obj &rest key
+(defun jsonaccess (obj &rest key)
   (jsonaccess_ obj key)
-)
-
-
-
-
-; (defun jsonaccess (obj fields)
-;   (if (stringp fields)
-;       (jsonaccess_ obj (list fields))
-;     (jsonaccess_ obj fields)))
-
-; (defun string-to-list (x)
-;   (if (stringp x)
-;       (list x)
-;     x))
-          
+  )
 
 (defun jsonaccess_ (obj fields)
   (if (null fields)
       obj
     (let* ((cur-key (first fields))
-           (current (cond ((and (integerp cur-key) 
-                                (eq (first obj) 'jsonarray))
-                           (nth (+ cur-key 1) obj)) 
-                          ((and (stringp cur-key) 
-                                (eq (first obj) 'jsonobj))
-                           (second (assoc cur-key (rest obj) :test #'string=)))
-                          (t (error "not a JSON object or array or not appropriate key" obj cur-key)))))
+           (current 
+            (cond ((and (integerp cur-key) 
+                        (eq (first obj) 'jsonarray))
+                   (nth (+ cur-key 1) obj)) 
+                  ((and (stringp cur-key) 
+                        (eq (first obj) 'jsonobj))
+                   (second (assoc cur-key
+                                  (rest obj) :test #'string=)))
+                  (t (error
+                      "not a JSON object or array or not appropriate key"
+                      obj cur-key)))))
       (jsonaccess_ current (rest fields))))) 
 
-;chiamata corretta
-(defun j (obj &rest fields)
-  (jsonaccess_ obj fields)
-)
 
