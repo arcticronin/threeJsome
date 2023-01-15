acdd83190ecfe79427b3a2aa9d3c631345e9a62c
arcticronin <ronin.git@gmail.com>
2023-01-15 01:37:52 +0100

    jsonaccess corretto

--- lisp/jsonaccessnew.lisp
+++ lisp/jsonaccessnew.lisp
@@ -1,15 +1,24 @@
-(defun jsonaccess (obj fields)
-  (if (stringp fields)
-      (jsonaccess-from-list obj (list fields))
-    (jsonaccess-from-list obj fields)))
 
-(defun string-to-list (x)
-  (if (stringp x)
-      (list x)
-    x))
+
+(defun jsonaccess obj &rest key
+  (jsonaccess_ obj key)
+)
+
+
+
+
+; (defun jsonaccess (obj fields)
+;   (if (stringp fields)
+;       (jsonaccess_ obj (list fields))
+;     (jsonaccess_ obj fields)))
+
+; (defun string-to-list (x)
+;   (if (stringp x)
+;       (list x)
+;     x))
           
 
-(defun jsonaccess-from-list (obj fields)
+(defun jsonaccess_ (obj fields)
   (if (null fields)
       obj
     (let* ((cur-key (first fields))
@@ -20,10 +29,10 @@
                                 (eq (first obj) 'jsonobj))
                            (second (assoc cur-key (rest obj) :test #'string=)))
                           (t (error "not a JSON object or array or not appropriate key" obj cur-key)))))
-      (jsonaccess-from-list current (rest fields))))) 
+      (jsonaccess_ current (rest fields))))) 
 
 ;chiamata corretta
 (defun j (obj &rest fields)
-  (jsonaccess-from-list obj fields)
+  (jsonaccess_ obj fields)
 )
 
