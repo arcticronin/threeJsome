9d00ff44500514cc652b4a7d06222aca4e6c1287
Tmont28 <61681306+Tmont28@users.noreply.github.com>
2023-01-15 22:30:00 +0100

    Update jsonparse.lisp

--- Consegna/Lisp/jsonparse.lisp
+++ Consegna/Lisp/jsonparse.lisp
@@ -1,1 +1,19 @@
+;;; Membri del Gruppo
+;;; 856095 Manzi Luca 
+;;; 851849 Montoli Matteo
+;;; 859246 Zhou Chengjie
 
+;;; -*- Mode: Lisp -*-
+
+;;; begin-of-file jsonparsing.lisp
+
+
+	
+	 
+
+
+
+
+
+
+;;; end-of-file jsonparsing.lisp
